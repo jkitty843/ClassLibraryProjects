4116a9ed91fbef4f9db068de14610c6a72161548
jcaitlynaltman@gmail.com <jcaitlynaltman@gmail.com>
2023-05-18 20:52:40 -0400

    Create StartsWithUpper extension method

--- StringLibrary/Class1.vb
+++ StringLibrary/Class1.vb
@@ -1,3 +1,15 @@
-Public Class Class1
+Imports System.Runtime.CompilerServices
 
-End Class
+Namespace UtilityLibraries
+    Public Module StringLibrary
+        <Extension>
+        Public Function StartsWithUpper(str As String) As Boolean
+            If String.IsNullOrWhiteSpace(str) Then
+                Return False
+            End If
+
+            Dim ch As Char = str(0)
+            Return Char.IsUpper(ch)
+        End Function
+    End Module
+End Namespace
